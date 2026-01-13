8f131a03beb24fbbe563d45730b0486e8fe339c5
BruntoChola <mohdayaankhan200@gmail.com>
2026-01-14 02:41:04 +0530

    organised the files and added checkpoints

---
+++ Assets/Scripts/raceSystem/checkpoints.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+[RequireComponent(typeof(Collider))]
+public class checkpoints : MonoBehaviour
+{
+    public int checkpointIndex;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
